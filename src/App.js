60d6472d48de859e0264006adc029be10a02367f
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-10 10:30:46 +0700

    create form modal component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
 } from "firebase/firestore";
 import { currencyFormat } from "./helpers/currency-format";
 import ListCard from "./components/ListCard";
+import FormModal from "./components/FormModal";
 
 function App() {
   const [isShowModal, setIsShowModal] = useState(false);
@@ -193,67 +194,12 @@ function App() {
       </div>
 
       {isShowModal && (
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <div className="mx-2 w-100">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">
-                  {formData.id == null ? "Tambah" : "Edit"} Item
-                </h5>
-              </div>
-
-              <div className="px-3 mb-3">
-                <p className="p-0 m-0">Nama Item</p>
-                <input
-                  className="form-control form-control-sm"
-                  type="text"
-                  value={formData.item}
-                  name="item"
-                  onChange={(e) => updateFormData(e)}
-                />
-              </div>
-
-              <div className="px-3 mb-3">
-                <p className="p-0 m-0">Jumlah</p>
-                <input
-                  className="form-control form-control-sm"
-                  type="number"
-                  value={formData.value}
-                  name="value"
-                  onChange={(e) => updateFormData(e)}
-                />
-              </div>
-
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  onClick={() => setIsShowModal(false)}
-                >
-                  Tutup
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  onClick={onSubmit}
-                >
-                  Simpan
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <FormModal
+          formData={formData}
+          updateFormData={updateFormData}
+          setIsShowModal={setIsShowModal}
+          onSubmit={onSubmit}
+        />
       )}
     </div>
   );
---
+++ src/components/FormModal.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const FormModal = ({ formData, updateFormData, setIsShowModal, onSubmit }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div className="mx-2 w-100">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">
+              {formData.id == null ? "Tambah" : "Edit"} Item
+            </h5>
+          </div>
+
+          <div className="px-3 mb-3">
+            <p className="p-0 m-0">Nama Item</p>
+            <input
+              className="form-control form-control-sm"
+              type="text"
+              value={formData.item}
+              name="item"
+              onChange={(e) => updateFormData(e)}
+            />
+          </div>
+
+          <div className="px-3 mb-3">
+            <p className="p-0 m-0">Jumlah</p>
+            <input
+              className="form-control form-control-sm"
+              type="number"
+              value={formData.value}
+              name="value"
+              onChange={(e) => updateFormData(e)}
+            />
+          </div>
+
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => setIsShowModal(false)}
+            >
+              Tutup
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={onSubmit}
+            >
+              Simpan
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormModal;
