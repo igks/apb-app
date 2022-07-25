849d11568b37c77d1b4675890d33ca8185d53b80
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-07-25 08:37:21 +0700

    remove approval button in list card

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { currencyFormat } from "../helpers/currency-format";
 import { useNavigate } from "react-router-dom";
 
 const ListCard = ({ record, onToggleStatus, onUpdate, onDelete }) => {
   const navigate = useNavigate();
 
+  const [used, setUsed] = useState(0);
+
   const goToDetail = (record) => {
     navigate("/detail", { state: record });
   };
+
+  useEffect(() => {
+    if (record?.details?.length > 0 ?? false) {
+      let newUsedValue = 0;
+      record.details.forEach((item) => {
+        newUsedValue += item.value;
+      });
+      setUsed(newUsedValue);
+    }
+  }, []);
 
   return (
     <div
@@ -17,27 +29,29 @@ const ListCard = ({ record, onToggleStatus, onUpdate, onDelete }) => {
         borderRadius: 10,
       }}
       className={
-        record.isApprove
-          ? "d-flex flex-row justify-content-between alert alert-success"
-          : "d-flex flex-row justify-content-between alert alert-danger"
+        record.isApprove ? "alert alert-success" : "alert alert-danger"
       }
       role="alert"
     >
-      <div className="p-1">
+      <div className="p-0">
         <h6 className="p-0 m-0">{record.item}</h6>
-        <p className="m-0 p-0">Rp. {currencyFormat(record.value)}</p>
-        <p className="p-0 m-0">{record.isApprove ? "Approve" : "Pending"}</p>
+        <p className="m-0 p-0">Anggaran Rp. {currencyFormat(record.value)}</p>
+        <p className="m-0 p-0 text-danger">
+          Sisa Rp. {currencyFormat(record.value - used)}
+        </p>
+        {/* <p className="p-0 m-0">{record.isApprove ? "Approve" : "Pending"}</p> */}
+      </div>
+      <hr className="mt-1" />
+      <div className="m-0 p-0 d-flex flex-row justify-content-around">
         <div className="badge bg-warning" onClick={() => goToDetail(record)}>
           Detail
         </div>
-      </div>
-      <div className="m-0 p-0 d-flex flex-column justify-content-between">
-        <div
+        {/* <div
           className="badge bg-primary"
           onClick={() => onToggleStatus(record.id, !record.isApprove)}
         >
           {record.isApprove ? "Pending" : "Approve"}
-        </div>
+        </div> */}
         <div className="badge bg-info" onClick={() => onUpdate(record)}>
           Update
         </div>
