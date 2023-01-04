8b8c154fd61d3aa6855b2de2e819c66786865445
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2023-01-04 15:46:52 +0700

    use container in app.js

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Menu from "./pages/Menu";
+import Anggaran from "./pages/Anggaran";
+import Detail from "./pages/Detail";
+import Catatan from "./pages/Catatan";
+import PerTanggal from "./pages/PerTanggal";
+import GraphHarian from "./pages/GraphHarian";
+import MyChart from "./pages/Chart";
+
+export const appRoutes = (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Menu />}></Route>
+      <Route path="/anggaran" element={<Anggaran />}></Route>
+      <Route path="/detail" element={<Detail />}></Route>
+      <Route path="/catatan" element={<Catatan />}></Route>
+      <Route path="/pertanggal" element={<PerTanggal />}></Route>
+      <Route path="/graph-harian" element={<GraphHarian />}></Route>
+      <Route path="/chart" element={<MyChart />}></Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </Router>
+);
