4ce45e982a73cb04181a302cd51484809277dfc7
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2023-01-10 21:33:26 +0700

    convert anggaran header to styled component

---
+++ src/components/anggaran/AnggaranHeader/styled.component.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: ${({ mb }) => mb};
+`;
+
+export const Card = styled.div`
+  background-color: ${({ color }) => color};
+  border-radius: 5px;
+  padding: 5px 10px;
+  height: fit-content;
+  width: 49%;
+`;
+
+export const Divider = styled.hr`
+  margin: 5px 0;
+`;
+
+export const Title = styled.h6`
+  text-align: center;
+  margin: 0;
+`;
