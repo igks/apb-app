f94a4f4d8852b0c1926f69c795fc81b27d4aa3b9
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2023-01-10 20:28:05 +0700

    convert select month to styled component

--- src/components/anggaran/SelectMonth/index.jsx
+++ src/components/anggaran/SelectMonth/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { optionBulan } from "constants";
+import * as S from "./styled.component";
 
 const SelectMonth = ({ onSetMonth }) => {
   const bulanOnChange = (e) => {
@@ -11,19 +12,15 @@ const SelectMonth = ({ onSetMonth }) => {
   };
 
   return (
-    <div className="row g-3 align-items-center justify-content-between mb-3">
-      <div className="col">
-        <label className="col-form-label">Pilih bulan</label>
-      </div>
-      <div className="col-8 ">
-        <input
-          type="month"
-          id="month"
-          className="form-control"
-          onChange={bulanOnChange}
-        />
-      </div>
-    </div>
+    <S.Container>
+      <S.Label>Pilih Bulan</S.Label>
+      <S.Input
+        type="month"
+        id="month"
+        className="form-control"
+        onChange={bulanOnChange}
+      />
+    </S.Container>
   );
 };
 
