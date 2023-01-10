074bd0938870890efc03cf2e3d686e9e33dd8a26
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2023-01-10 20:28:05 +0700

    convert select month to styled component

---
+++ src/components/anggaran/SelectMonth/styled.component.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const Label = styled.div`
+  width: 30%;
+`;
+
+export const Input = styled.input`
+  width: 70%;
+  border: 1px solid #99dce5;
+`;
