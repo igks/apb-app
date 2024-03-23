79af0375b1fc3f7fff4373a6bdab79d65919f187
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2024-03-23 11:03:59 +0700

    setup firebase hosting

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGESENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyBrdFIkjyZEPKbqe1Tl7DxAuoL9Ix0lKm4",
+  authDomain: "apb-app-63a10.firebaseapp.com",
+  projectId: "apb-app-63a10",
+  storageBucket: "apb-app-63a10.appspot.com",
+  messagingSenderId: "844670029576",
+  appId: "1:844670029576:web:ed619732ad80d5428266bd",
 };
 
 const app = initializeApp(firebaseConfig);
