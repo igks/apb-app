9579a4a0f9831de8fcd3c6e169cbd66a0742243c
igks <sugiantara.igk@gmail.com>
2025-09-29 03:34:50 +0700

    update cloudinary image preview

--- src/components/cloud-preview/index.jsx
+++ src/components/cloud-preview/index.jsx
@@ -3,7 +3,13 @@ import { Image, CloudinaryContext } from "cloudinary-react";
 function CloudPreview({publicId}) {
   return (
     <CloudinaryContext cloudName="dphyecnpy">
-      <Image publicId={publicId} width="300" crop="scale" />
+      <Image publicId={publicId} 
+        // width="auto"
+        crop="scale"
+        quality="auto"
+        dpr="auto" 
+        style={{ width: "100%", height: "auto" }}
+        />
     </CloudinaryContext>
   )
 }
